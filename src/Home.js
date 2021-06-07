2b89e6071d931b39f6f010d81cde2279b5c7bfed
Yashakk <yashashree.kulkarni05@gmail.com>
2021-06-07 15:23:36 -0400

    second commit

---
+++ src/Home.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import AppNavbar from './Navbar';
+import { Button, Container } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <AppNavbar />
+                <Container fluid>
+                    <Button className="m-5 nav bg-light">
+                        <Link to="/inventories"
+                            className="nav-link">
+                            Manage Inventory List
+                        </Link>
+                    </Button>
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-useless-constructor */
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default class AppNavbar extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <Navbar className="navbar-link bg-dark px-5" expand="md">
+                <NavbarBrand tag={Link} to="/">Home</NavbarBrand>
+            </Navbar>
+        )
+    }
+}
